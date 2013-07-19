d019bd8156ede579b6beee2766c9ceffaf97a3f1
Bryant Hughes <bryant@thegoodlab.com>
2013-07-18 19:16:02 -0500

    almost have the grunt file setup correctly

---
+++ app/src/js/main.js
@@ -0,0 +1,14 @@
+if (jQuery){
+  console.log('jQuery Loaded');
+}
+if(_){
+  console.log('Underscore Loaded');
+}
+if(Backbone){
+  console.log('Backbone Loaded');
+}
+if(Backbone.Marionette){
+  console.log('Backbone Marionette Loaded');
+}
+
+conosle.log("asdf");
