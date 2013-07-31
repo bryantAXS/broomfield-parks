dfd2f0177677a1c50f64425f8090c0939f34e10a
Chris Arnold <chris@limbercreative.com>
2013-07-31 17:13:41 -0600

    layout and style updates

---
+++ public/scripts/bower_modules/in-viewport/test/no-watch.js
@@ -0,0 +1,40 @@
+describe('without using callbacks', function() {
+
+  describe('when element is visible', function() {
+    var result;
+    var test = createTest();
+
+    before(function() {
+      insertTest(test);
+      result = inViewport(test);
+    });
+
+    it('gives a positive result immediately', function() {
+      assert(result === true);
+    });
+
+    after(clean(test));
+  });
+
+  describe('when element is not visible', function() {
+    var result;
+    var test = createTest({
+      style: {
+        position: 'relative',
+        top: '15000px'
+      }
+    });
+
+    before(function() {
+      insertTest(test);
+      result = inViewport(test);
+    });
+
+    it('gives a negative result immediately', function() {
+      assert(result === false);
+    });
+
+    after(clean(test));
+  });
+
+});
