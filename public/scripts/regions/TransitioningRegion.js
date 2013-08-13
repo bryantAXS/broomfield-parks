4aa65fa43808a0b701260a8f9e6e71510ce749fe
Bryant Hughes <bryant@thegoodlab.com>
2013-08-12 21:26:19 -0500

    updating sliding animations for enter and close of park details view

--- public/scripts/regions/TransitioningRegion.js
+++ public/scripts/regions/TransitioningRegion.js
@@ -42,30 +42,9 @@ define([
       var self = this;
 
       if(view.el.id === "park-detail-layout-container"){
-
-        //create a callback used to determine when the new view is added to the screen
-        this.hasOpened = $.Deferred();
-
-        // if we're about to show the park detail page, let's transition that in
-        this.$el.append(view.el);
-        this.$el.css({
-          translate: ["-100%", 0],
-          opacity: 0
-        });
-
-        this.$el.transition({
-          x: 0,
-          opacity: 1
-        }, function(){
-          App.vent.trigger("page:opened");
-          self.hasOpened.resolve();
-        });
-
+        this.openSlideFromLeft(view);
       }else{
-
-        this.$el.append(view.el);
-        App.vent.trigger("page:opened");
-
+        this.openNoTransition(view);
       }
 
     },
@@ -83,6 +62,75 @@ define([
       if (!view || view.isClosed){
         return;
       }
+
+      if(view.el.id === "park-detail-layout-container"){
+        this.closeSlideToLeft(view);
+      }else{
+        this.closeSlideToRight(view);
+      }
+
+
+      // we need to make sure this is deleted right away and
+      // not after the callback
+      delete this.currentView;
+
+    },
+
+    /**
+     * A helper to actualy close and views and remove them from the dom.
+     * @param  {[type]} view [description]
+     * @return {[type]}      [description]
+     */
+    closeView: function(view){
+
+      //call 'close' or 'remove', depending on which is found
+      if(view.close){
+        view.close();
+      }else if(view.remove){
+        view.remove();
+      }
+
+      Backbone.Marionette.triggerMethod.call(this, "close");
+
+    },
+
+
+    //
+    // TRANSITIONS
+    //
+
+    openSlideFromLeft: function(view){
+
+      var self = this;
+
+      //create a callback used to determine when the new view is added to the screen
+      this.hasOpened = $.Deferred();
+
+      // if we're about to show the park detail page, let's transition that in
+      this.$el.append(view.el);
+      this.$el.css({
+        translate: ["-100%", 0],
+        opacity: 0
+      });
+
+      this.$el.transition({
+        x: 0,
+        opacity: 1
+      }, function(){
+        App.vent.trigger("page:opened");
+        self.hasOpened.resolve();
+      });
+
+    },
+
+    openNoTransition: function(view){
+      this.$el.append(view.el);
+      App.vent.trigger("page:opened");
+    },
+
+    closeSlideToRight: function(view){
+
+      var self = this;
 
       //create a callback used to determine when the new view is added to the screen
       this.hasClosed = $.Deferred();
@@ -107,29 +155,39 @@ define([
 
       });
 
-      // we need to make sure this is deleted right away and
-      // not after the callback
-      delete this.currentView;
-
     },
 
-    /**
-     * A helper to actualy close and views and remove them from the dom.
-     * @param  {[type]} view [description]
-     * @return {[type]}      [description]
-     */
-    closeView: function(view){
+    closeSlideToLeft: function(view){
 
-      //call 'close' or 'remove', depending on which is found
-      if(view.close){
-        view.close();
-      }else if(view.remove){
-        view.remove();
-      }
+      var self = this;
 
-      Backbone.Marionette.triggerMethod.call(this, "close");
+      //create a callback used to determine when the new view is added to the screen
+      this.hasClosed = $.Deferred();
+
+      // determining which direction we should move
+      var xPos = "-100%";
+
+      // this is where the transition happens
+      this.$el.transition({
+        x: xPos,
+        opacity: 0
+      }, function(){
+
+        self.$el.css({
+          translate: [0,0],
+          opacity:1
+        });
+        self.closeView(view);
+
+        // this needs to be at the end, so we don't trigger an open animation before ouro final styles are set a few lines above.
+        self.hasClosed.resolve();
+
+      });
 
     }
+
+
+
 
   });
 
