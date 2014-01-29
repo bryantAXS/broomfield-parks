30ecd8493a16d4c74633c59ac5a5f66ebe893231
Bryant Hughes <bryant@thegoodlab.com>
2014-01-29 11:43:06 -0600

    adding analytics

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -48,6 +48,17 @@
       var allParksJSON = {{ $parksJSON }}
     </script>
 
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-23070397-19', 'broomfield.org');
+      ga('send', 'pageview');
+
+    </script>
+
     @if ($environment == "local")
       <script data-main="/scripts/main.js" src="/scripts/vendor/require.js"></script>
     @else
